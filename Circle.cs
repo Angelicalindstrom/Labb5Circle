f9de861678f12bec5871f22193aa1eb961ff0edf
Angelica Lindström <nutrition.angelica@gmail.com>
2023-10-15 20:55:23 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb5Circle
+{
+    // Angelica Lindström NET.23
+    internal class Circle
+    { // declared class, named it Circle
+        
+        //declare variables (private)
+        private double _pi = Math.PI;
+        private int _Radius;
+        private string _name;
+      
+        
+        public Circle(string name, int radius)  // Construktor, parameters name and radius
+        {
+            
+            _name = name;
+            _Radius = radius;
+            Console.WriteLine(_name + " Skapades och har en radie på " + _Radius );
+        }
+
+        public double GetArea() // method that calculates the area of the circle
+        {
+            double circleArea = (_Radius * _Radius) * _pi; 
+            Console.Write($"Räknar ut {_name} area, som blir : " );
+            return circleArea;
+        } 
+        public double GetPerimeter()   // method that calculates the perimeter of the circle
+        {
+            double circlePerimeter = (_Radius + _Radius) * _pi;
+            Console.Write($"Räknar ut {_name} omkrets, som blir : ");
+            return circlePerimeter;
+        }
+
+        public double GetSphereVolume()  // method that calculates the volume of the sphere
+        {
+            double sphereVolume = ((4 * _pi) * (_Radius * _Radius * _Radius)) /3;
+            Console.Write($"Räknar ut {_name} volym, som om det vore en sfär, då blir det : ");
+            return sphereVolume;
+        }
+
+        public double GetSphereArea()  // method that calculates the area of the sphere
+        {
+            double sphereArea = (4 * _pi) * (_Radius * _Radius);
+            Console.Write($"Räknar ut {_name} area, som om det vore en sfär, då blir det : ");
+            return sphereArea;
+        }
+     
+        
+        // used double in all my methods so the value of the calculation would be right
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Labb5Circle
+{
+    internal class Program
+    {
+
+        // Angelica Lindström NET23
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Programmet skapar två cirklar:\n");
+           
+
+
+            Circle circle1 = new Circle("Cirkel Nr1", 5); // creates the object named cirkle nr1 with the radius of 5.
+            Console.WriteLine(circle1.GetArea()); // Call the method that counts the Area of the circle
+            Console.WriteLine(circle1.GetPerimeter()); // Call the method that counts the Perimeter of the circle
+            Console.WriteLine(circle1.GetSphereVolume()); // Call the method that counts the volume of the cirkle as if its a sphere
+            Console.WriteLine(circle1.GetSphereArea());  // Call the method that counts the are of the cirkle as if its a sphere
+
+            Console.WriteLine(" ");
+
+            // Same methods 
+
+            Circle circle2 = new Circle("Cirkel Nr2", 6);   // creates the object named cirkle nr2 with the radius of 5.
+            Console.WriteLine(circle2.GetArea());  // Call the method that counts the Area of the circle
+            Console.WriteLine(circle2.GetPerimeter());  // Call the method that counts the Perimeter of the circle
+            Console.WriteLine(circle2.GetSphereVolume());   // Call the method that counts the volume of the cirkle as if its a sphere
+            Console.WriteLine(circle2.GetSphereArea());  // Call the method that counts the are of the cirkle as if its a sphere
+
+
+
+
+
+
+            // Hur avrundar jag till 3 decimaler?
+        }
+    }
+
+}
